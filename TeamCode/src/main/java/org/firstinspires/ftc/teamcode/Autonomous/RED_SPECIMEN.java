634437813304d24a4a8ca0dabedec4df472fffe8
Dhee0104 <168314704+Dhee0104@users.noreply.github.com>
2024-11-04 09:41:53 -0500

    Finished all autonomus outtake
    Finished implementing outtake functions as actions and executing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RED_SPECIMEN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RED_SPECIMEN.java
@@ -26,11 +26,56 @@ import org.firstinspires.ftc.teamcode.tuningVal.MecanumDrive;
 public class RED_SPECIMEN extends LinearOpMode {
     // need to create action classes
 
+    public class Out{
+        OuttakeSystem out = new OuttakeSystem(hardwareMap);
+
+        public class chamber1 implements Action {
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+               out.topBarPart1();
+               return false;
+            }
+        }
+        public Action chamber1(){return new chamber1();}
+
+        public class chamber2 implements Action {
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                out.topBarPart2();
+                return false;
+            }
+        }
+        public Action chamber2(){return new chamber2();}
+
+        public class accept1 implements Action {
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                out.pickUpPart1();
+                return false;
+            }
+        }
+        public Action accept1(){return new accept1();}
+
+        public class accept2 implements Action {
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                out.pickUpPart2();
+                return false;
+            }
+        }
+        public Action accept2(){return new accept2();}
+
+    }
     @Override
     public void runOpMode() {
         Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
-        OuttakeSystem out = new OuttakeSystem(hardwareMap);
+        Out out = new Out();
+        OuttakeSystem outfunc = new OuttakeSystem(hardwareMap);
         Vector2d entry = new Vector2d(35,-36);
         Vector2d s1 = new Vector2d(48,-10);
         Vector2d s2 = new Vector2d(58,-10);
@@ -43,7 +88,7 @@ public class RED_SPECIMEN extends LinearOpMode {
         Vector2d target4 =   new Vector2d(1,-40);
         Vector2d target5 =   new Vector2d(-2,-40);
         // vision here that outputs position
-        int visionOutputPosition = 1;
+
 
 
         TrajectoryActionBuilder startToBar = drive.actionBuilder(initialPose)
@@ -118,43 +163,62 @@ public class RED_SPECIMEN extends LinearOpMode {
                 .build();
 
         // actions that need to happen on init; for instance, a claw tightening.
-        out.init();
 
 
 
+        outfunc.init();
 
 
 
         Actions.runBlocking(
                 new SequentialAction(
                         //bar part 1
+                        out.chamber1(),
                        startToBar.build(),
                         //bar part 2
                         //accept part1
+                        out.chamber2(),
+                        out.accept1(),
                         barToPushToAccept.build(),
                         //accept part 2
                         //bar part 1
+                        out.accept2(),
+                        out.chamber1(),
                         acceptToTarget.build(),
                         //bar part 2
                         //accept part 1
+                        out.chamber2(),
+                        out.accept1(),
                         targetToAccept.build(),
                         //accept part 2
                         //bar part 1
+                        out.accept2(),
+                        out.chamber1(),
                         acceptToTarget2.build(),
                         //bar part 2
                         //accept part1
+                        out.chamber2(),
+                        out.accept1(),
                         targetToAccept.build(),
                         //accept part 2
                         //bar part 1
+                        out.accept2(),
+                        out.chamber1(),
                         acceptToTarget3.build(),
                         //bar part 2
                         //accept part1
+                        out.chamber2(),
+                        out.accept1(),
                         targetToAccept.build(),
                         //accept part 2
                         //bar part 1
+                        out.accept2(),
+                        out.chamber1(),
                         acceptToTarget4.build(),
                         //bar part 2
                         //accept part1
+                        out.chamber2(),
+                        out.accept1(),
                         trajectoryActionCloseOut
                 )
         );
