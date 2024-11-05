654d876505927376113a9670b6d6fc59a365895f
Dhee0104 <168314704+Dhee0104@users.noreply.github.com>
2024-11-05 10:29:10 -0500

    Create MotorPower.java
    MotorPower is a class to move the robot around. Make object then use. Params are both time and pwr

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorPowerAuton/MotorPower.java
@@ -0,0 +1,112 @@
+package org.firstinspires.ftc.teamcode.MotorPowerAuton;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class MotorPower  {
+    private  ElapsedTime time = new ElapsedTime();
+    private  DcMotor FL;//0
+    private  DcMotor BL;//1
+    private  DcMotor BR;//2
+    private  DcMotor FR;//3
+    public MotorPower(HardwareMap map) {
+        this.FL = (DcMotor) map.get("frontLeft");
+        this.FR = (DcMotor) map.get("frontLeft");
+        this.BR = (DcMotor) map.get("frontLeft");
+        this.BL = (DcMotor) map.get("frontLeft");
+        FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FL.setDirection(DcMotorSimple.Direction.REVERSE);
+        BL.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    public void back(double pwr, int t){
+        time.reset();
+        FR.setPower(-pwr);
+        BR.setPower(-pwr);
+        FL.setPower(-pwr);
+        BL.setPower(-pwr);
+        while (time.milliseconds() < t){
+
+        }
+        FR.setPower(0);
+        BR.setPower(0);
+        FL.setPower(0);
+        BL.setPower(0);
+    }
+    public void strafeR(double pwr, int t){
+        time.reset();
+        FR.setPower(-pwr);
+        BR.setPower(pwr);
+        FL.setPower(pwr);
+        BL.setPower(-pwr);
+        while (time.milliseconds() < t){
+
+        }
+        FR.setPower(0);
+        BR.setPower(0);
+        FL.setPower(0);
+        BL.setPower(0);
+    }
+    public void strafeL(double pwr, int t){
+        time.reset();
+        FR.setPower(pwr);
+        BR.setPower(-pwr);
+        FL.setPower(-pwr);
+        BL.setPower(pwr);
+        while (time.milliseconds() < t){
+
+        }
+        FR.setPower(0);
+        BR.setPower(0);
+        FL.setPower(0);
+        BL.setPower(0);
+    }
+    public void turnL(double pwr, int t){
+        time.reset();
+        FR.setPower(pwr);
+        BR.setPower(pwr);
+        FL.setPower(-pwr);
+        BL.setPower(-pwr);
+        while (time.milliseconds() < t){
+
+        }
+        FR.setPower(0);
+        BR.setPower(0);
+        FL.setPower(0);
+        BL.setPower(0);
+    }
+    public void turnR(double pwr, int t){
+        time.reset();
+        FR.setPower(-pwr);
+        BR.setPower(-pwr);
+        FL.setPower(pwr);
+        BL.setPower(pwr);
+        while (time.milliseconds() < t){
+
+        }
+        FR.setPower(0);
+        BR.setPower(0);
+        FL.setPower(0);
+        BL.setPower(0);
+    }
+    public void forw(double pwr, int t){
+        time.reset();
+        FR.setPower(pwr);
+        BR.setPower(pwr);
+        FL.setPower(pwr);
+        BL.setPower(pwr);
+        while (time.milliseconds() < t){
+
+        }
+        FR.setPower(0);
+        BR.setPower(0);
+        FL.setPower(0);
+        BL.setPower(0);
+    }
+
+
+}
