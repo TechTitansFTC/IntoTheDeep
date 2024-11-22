cbb3a22d03193e8f5f4a1c72cb02666e81e7cfd7
midK-night <pasum462@gmail.com>
2024-11-21 19:27:31 -0600

    update differential.java
    create basic movement for each of the slides (up down out in) and off

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/differential.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/differential.java
@@ -12,5 +12,28 @@ public class differential {
         this.motorR = (DcMotorEx) map.get("difR");
     }
 
-    
+    public void outtakeUp() {
+        motorR.setPower(-1);
+        motorL.setPower(1);
+    }
+
+    public void outtakeDown() {
+        motorR.setPower(1);
+        motorL.setPower(-1);
+    }
+
+    public void intakeOut() {
+        motorR.setPower(1);
+        motorL.setPower(1);
+    }
+
+    public void intakeIn() {
+        motorR.setPower(0);
+        motorL.setPower(0);
+    }
+
+    public void slidesOff() {
+        motorR.setPower(0);
+        motorL.setPower(0);
+    }
 }
