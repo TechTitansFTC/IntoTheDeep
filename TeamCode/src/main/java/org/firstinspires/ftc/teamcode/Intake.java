bbfa7961cd3cf2dbc11fd5e02a64786cac57b155
Dhee0104 <168314704+Dhee0104@users.noreply.github.com>
2024-12-04 18:11:45 -0600

    re-add elbow
    cuz krish is a bum
    
    Co-Authored-By: midK-night <130721380+midK-night@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -9,6 +9,7 @@ public class Intake {
     private CRServo geckoR;
     private Servo armL;
     private Servo armR;
+    private Servo elbow;
 
     //TODO: find intake servo values
     //TODO: finalize intake PIVs
@@ -16,11 +17,15 @@ public class Intake {
     private final double ARM_L_OUT = 0;
     private final double ARM_R_IN = 0;
     private final double ARM_R_OUT = 0;
+    private final double ELBOW_BASE = 0;
+    private final double ELBOW_EXTENDED = 0;
+
     public Intake (HardwareMap map) {
         this.geckoL = (CRServo) map.get("geckoL");
         this.geckoR = (CRServo) map.get("geckoR");
         this.armL = (Servo) map.get("intakeL");
         this.armR = (Servo) map.get("intakeR");
+        this.elbow = (Servo) map.get("intake");
     }
 
     public void init () {
@@ -28,6 +33,7 @@ public class Intake {
         geckoR.setPower(0);
         armL.setPosition(ARM_L_IN);
         armR.setPosition(ARM_R_IN);
+        elbow.setPosition(ELBOW_BASE);
     }
 
     public void intakeOn() {geckoL.setPower(-1);geckoR.setPower(1);}
@@ -44,4 +50,7 @@ public class Intake {
         armR.setPosition(ARM_R_OUT);
     }
 
+    public void elbowOut() {elbow.setPosition(ELBOW_EXTENDED);}
+
+    public void elbowIn() {elbow.setPosition(ELBOW_BASE);}
 }
